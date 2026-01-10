7db39bdac3471085e16f804f9ac8fe030cf8f650
yennii <yennianabelaevariani28@gmail.com>
2026-01-10 14:00:53 +0700

    new

--- kerjosam/routes/web.php
+++ kerjosam/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth Routes
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/profile', [AuthController::class, 'profile'])->name('profile')->middleware('auth');
